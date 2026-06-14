7c09540e80ff581b71ac01fafcd3ccbb3f54a4ee
dkno21 <dkno2106@gmail.com>
2026-06-14 17:06:34 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,18 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+    } // <--- Tutup defaultConfig di sini
+
+    // compileOptions dan kotlinOptions harus di luar defaultConfig
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
+
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
